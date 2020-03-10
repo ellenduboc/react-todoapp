34ff71eab0c071a469275b40221d0ca0f17d204a
Ellen Duboc <ellenduboc-fsa@hotmail.com>
2020-03-10 17:32:39 -0300

    Applying css

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../App.css';
 
 
 
@@ -13,6 +14,14 @@ handleNameChange = text => {
   this.setState({ name: text.target.value });
 }
 
+handleEmailChange = text => {
+  this.setState({ email: text.target.value });
+}
+
+handleSenhaChange = text => {
+  this.setState({ senha: text.target.value });
+}
+
 handleSubmit = () => {
   alert("Cadastro realizado!")
 }
@@ -21,11 +30,47 @@ handleSubmit = () => {
 
     return(
       <div>
-        <h2>Cadastro</h2>
+        <h2>CADASTRE-SE</h2>
       
           <form onSubmit={ this.handleSubmit }>
-            <input placeholder="Nome" type="text" onChange={this.handleNameChange}/>
-            <button type="submit">Salvar</button>
+            <div>
+              <span 
+              className="user">Nome de usuário:</span>
+            </div>
+
+            <div>
+              <input 
+              className='input' 
+              type="text" 
+              onChange={this.handleNameChange}/>
+            </div>
+
+            <div>
+              <span className="email">E-mail de cadastro:</span>
+            </div>
+
+            <div>
+              <input 
+              className='input'
+              type="text" 
+              onChange={this.handleEmailChange}/>
+            </div>
+
+            <div>
+              <span className="senha">Senha:</span>
+            </div>
+
+            <div>
+              <input 
+              type="text" 
+              onChange={this.handleSenhaChange}/>
+            </div>
+
+            <div >
+              <button className="salve" type="submit">Salvar</button>
+            </div>
+
+              
           </form>
 
       </div>
